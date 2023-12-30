1ea8861cd30c89e5e76c5a67489e76e3db604e9e
Terry A. Brooks, Jr. <Terry.Arthur@BrooksJr.com>
2023-12-30 13:24:20 -0600

    Alternative Approach"

--- Python_Cleaner.py
+++ Python_Cleaner.py
@@ -2,37 +2,50 @@
 
 import psycopg2 as psycopg
 from dotenv import load_dotenv
-import os
+import os'
+import queries
 load_dotenv()
 
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        
-        with psycopg.connect(conninfo=os.getenv("CONNECTION_STRING")) as conn:
 
-    # Open a cursor to perform database operations
-            with conn.cursor() as cur:
-
-        # Execute a command: this creates a new table
-                print("Clearing Old Requests...")
-                cur.execute("""
+def clean_up():
+    query = """
                     DELETE FROM requests_requests  
                     WHERE to_timestamp(time) > NOW() - INTERVAL "30 days";
-                    """)
+                    """
+    if queries.query(query, os.getenv("CONNECTION_STRING")):
+        return "Cleaned Old Request Data"
+    else:
+        raise RuntimeError("Unabled to Clean!")
 
-        # display the PostgreSQL database server version
+# def connect():
+    # """ Connect to the PostgreSQL database server """
+    # conn = None
+    # try:
+    #     # read connection parameters
+        
+    #     with psycopg.connect(conninfo=os.getenv("CONNECTION_STRING")) as conn:
+
+    # # Open a cursor to perform database operations
+    #         with conn.cursor() as cur:
+
+    #     # Execute a command: this creates a new table
+    #             print("Clearing Old Requests...")
+    #             cur.execute("""
+    #                 DELETE FROM requests_requests  
+    #                 WHERE to_timestamp(time) > NOW() - INTERVAL "30 days";
+    #                 """)
+
+    #     # display the PostgreSQL database server version
        
-	# close the communication with the PostgreSQL
-    except (Exception, psycopg .DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.commit()
-            print('Database connection closed.')
+	# # close the communication with the PostgreSQL
+    # except (Exception, psycopg .DatabaseError) as error:
+    #     print(error)
+    # finally:
+    #     if conn is not None:
+    #         conn.commit()
+    #         print('Database connection closed.')
 
 
 if __name__ == '__main__':
-    connect()
+    # connect()
+    clean_up()
